84c166dfd10f8007c6cd0d2169a615078fb10b57
unknown <wangrogerc@gmail.com>
2023-07-20 16:59:38 -0700

    removed unused languages

--- typescript/Employee.ts
+++
@@ -1,9 +0,0 @@
-import Name from './Name';
-
-class Employee {
-  constructor(private readonly identifier: number, private readonly name: Name) { }
-
-  toString = () => `${this.name.toString()}: ${this.identifier}`;
-}
-
-export default Employee;
--- typescript/Name.ts
+++
@@ -1,7 +0,0 @@
-class Name {
-  constructor(private readonly first: string, private readonly last: string) { }
-
-  toString = () => `${this.first} ${this.last}`;
-}
-
-export default Name;
